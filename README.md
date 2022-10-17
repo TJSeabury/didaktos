9ed4f0f7ea967129a17dfc1dbdad2fcea93aac10
Tyler Seabury <tylerseabury@gmail.com>
2022-10-17 13:09:34 -0400

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# Didaktos
+
+A simple open-source LMS (Learning Management System).
---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,30 @@
+import NextAuth, { NextAuthOptions } from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import FacebookProvider from "next-auth/providers/facebook"
+import GithubProvider from "next-auth/providers/github"
+import TwitterProvider from "next-auth/providers/twitter"
+import Auth0Provider from "next-auth/providers/auth0"
+
+// For more information on each option (and a full list of options) go to
+// https://next-auth.js.org/configuration/options
+export const authOptions: NextAuthOptions = {
+  // https://next-auth.js.org/configuration/providers/oauth
+  providers: [
+    Auth0Provider({
+      clientId: process.env.AUTH0_ID,
+      clientSecret: process.env.AUTH0_SECRET,
+      issuer: process.env.AUTH0_ISSUER,
+    }),
+  ],
+  theme: {
+    colorScheme: "light",
+  },
+  callbacks: {
+    async jwt({ token }) {
+      token.userRole = "admin"
+      return token
+    },
+  },
+}
+
+export default NextAuth(authOptions)
